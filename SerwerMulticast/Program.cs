b4c14a9db44345773fff86b1fe1423ef5b6d7841
Piotrek <piotrek120017@gmail.com>
2017-11-26 00:18:47 +0100

    initial commit

---
+++ SerwerMulticast/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SerwerMulticast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress multicastAddr = IPAddress.Parse("239.0.0.222");
+            UdpClient udpClient = new UdpClient();
+            udpClient.JoinMulticastGroup(multicastAddr);
+            IPEndPoint remoteEp = new IPEndPoint(multicastAddr, 2222);
+            byte[] buffer= Encoding.UTF8.GetBytes("test");
+            udpClient.Send(buffer, buffer.Length, remoteEp);
+        }
+    }
+}
